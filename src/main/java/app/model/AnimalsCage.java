b6af0041ffee3045d723895c21e7a7784e420d02
Anton Garussky <www.snowfall@mail.ru>
2021-09-11 23:15:54 +0300

    complete task 2.1.3

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,14 +1,24 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
     @Autowired
+    @Qualifier("dogBean")
     private Animal animal;
 
+    @Autowired
+    private Timer timer;
+
+    public Timer getTimer() {
+        return timer;
+    }
+
+    @Autowired
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
